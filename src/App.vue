1b6d8d8825bb186ad084b94fc861a638310cf029
Dorin Stanciu <stanciudorinmarian@gmail.com>
2021-06-28 22:42:10 +0300

    App layout & navigation bar created

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-    
+    <div id="app">
+        <component :is="layout" />
+    </div>
 </template>
 
 <script>
+    const DefaultLayout = () => import('./layouts/DefaultLayout');
 
     export default {
-        name: 'App',
+        name: "App",
 
-        data: () => ({
-            //
-        }),
+        computed: {
+            layout() {
+                for (const route of this.$route.matched) {
+                    if (route.meta.layout) {
+                        return route.meta.layout;
+                    }
+                }
+
+                return DefaultLayout;
+            },
+        }
     };
 </script>
---
+++ src/components/AppNavigationBar/AppNavigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar
+            app
+            color="primary"
+            dark
+    />
+</template>
+
+<script>
+    export default {
+        name: "AppNavigationBar"
+    };
+</script>
